e744012459464f37e030915430dc0200938a2b36
Dayron Romero <dr@dfvz.org>
2024-07-07 01:59:35 +0000

    [UPD] odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Selecciona la versión de Odoo que deseas instalar:"
+# Clear the screen for a cleaner execution view
+clear
+
+echo -e "\033[1;34m=== Actualización del Sistema Operativo ===\033[0m"
+sudo apt-get update && sudo apt-get upgrade -y
+
+echo -e "\033[1;34m=== Selección de Versión de Odoo ===\033[0m"
 echo "1) Odoo 15.0"
 echo "2) Odoo 16.0"
 echo "3) Odoo 17.0"
@@ -17,90 +23,95 @@ case $version_choice in
         odoo_version="17.0"
         ;;
     *)
-        echo "Opción no válida. Por favor, intenta nuevamente."
+        echo -e "\033[1;31mOpción no válida. Por favor, intenta nuevamente.\033[0m"
         exit 1
         ;;
 esac
 
-os_name=$(grep '^ID=' /etc/os-release | cut -d '=' -f2 | tr -d '"')
-
+os_name=$(source /etc/os-release && echo $ID)
+echo -e "\n\033[1;34m=== Verificación de Sistema Operativo ===\033[0m"
 if [[ "$os_name" == "ubuntu" || "$os_name" == "debian" || "$os_name" == "centos" ]]; then
     echo "Sistema operativo compatible detectado: $os_name."
 else
-    echo "Este script solo es compatible con Debian, Ubuntu o CentOS."
+    echo -e "\033[1;31mEste script solo es compatible con Debian, Ubuntu o CentOS.\033[0m"
     exit 1
 fi
 
-# Crear estructura de directorios en /opt/odoo
-echo "Creando estructura de directorios para Odoo en /opt/odoo..."
+echo -e "\n\033[1;34m=== Creación de Estructura de Directorios y Descompresión de Archivos ===\033[0m"
 sudo mkdir -p /opt/odoo/{config,extra-addons,source/odoo,source/enterprise}
-sudo chown -R $USER:$USER /opt/odoo
-echo "Directorios creados y permisos establecidos."
-
-# Verificar si GitHub CLI está instalado
-if ! command -v gh &> /dev/null; then
-    echo "Instalando GitHub CLI..."
-    if [[ "$os_name" == "debian" || "$os_name" == "ubuntu" ]]; then
-        sudo apt update
-        sudo apt install -y software-properties-common
-        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-        sudo apt-add-repository https://cli.github.com/packages
-        sudo apt update
-        sudo apt install gh
-    elif [[ "$os_name" == "centos" ]]; then
-        sudo yum install -y yum-utils
-        sudo yum config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-        sudo yum install gh
-    fi
-    echo "GitHub CLI ha sido instalado correctamente."
+if [ -f "Odoo.zip" ]; then
+    sudo unzip Odoo.zip -d /opt/
+    echo "Carpeta Base Odoo Creada Satisfactoriamente."
 else
-    echo "GitHub CLI ya está instalado."
+    echo -e "\033[1;31mNo se encontró el archivo Odoo.zip. Asegúrate de que el archivo esté en el directorio actual.\033[0m"
+    exit 1
 fi
 
-# Verificar si Docker y Docker Compose ya están instalados
-if command -v docker > /dev/null && command -v docker-compose > /dev/null; then
-    echo "Docker y Docker Compose ya están instalados. Tu sistema parece estar al día."
-else
-    # Instalación de Docker
+echo -e "\n\033[1;34m=== Instalación de Docker desde el Repositorio Oficial ===\033[0m"
+if ! command -v docker > /dev/null; then
     echo "Instalando Docker..."
-    if [[ "$os_name" == "debian" || "$os_name" == "ubuntu" ]]; then
+    if [[ "$os_name" == "ubuntu" || "$os_name" == "debian" ]]; then
         sudo apt-get update
         sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         sudo apt-get update
-        sudo apt-get install -y docker-ce
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     elif [[ "$os_name" == "centos" ]]; then
         sudo yum install -y yum-utils device-mapper-persistent-data lvm2
         sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-        sudo yum install -y docker-ce
+        sudo yum install -y docker-ce docker-ce-cli containerd.io
     fi
     sudo systemctl start docker
     sudo systemctl enable docker
-    echo "Docker ha sido instalado y configurado correctamente."
+    echo "Docker instalado y configurado correctamente."
+else
+    echo "Docker ya está instalado."
+fi
 
-    # Instalación de Docker Compose
-    echo "Instalando Docker Compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo -e "\n\033[1;34m=== Instalación de Docker Compose ===\033[0m"
+if ! command -v docker-compose > /dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    echo "Docker Compose ha sido instalado correctamente."
+    echo "Docker Compose instalado correctamente."
+else
+    echo "Docker Compose ya está instalado."
 fi
 
-# Preguntar al usuario si desea instalar Odoo Enterprise
-read -p "¿Deseas instalar Odoo en su versión Enterprise? (s/n): " install_enterprise
-install_enterprise=${install_enterprise,,} # Convertir a minúsculas
+echo -e "\n\033[1;34m=== Selección de Edición de Odoo ===\033[0m"
+echo "1) Community (Gratuita y Open Source)"
+echo "2) Enterprise (Requiere un código de licencia y es de pago)"
+read -p "Selecciona la edición que deseas instalar (1, 2): " edition_choice
 
-if [[ "$install_enterprise" == "s" ]]; then
-    echo "Autenticación necesaria para acceder al repositorio de Odoo Enterprise."
-    gh auth login
-    echo "Clonando el repositorio de Odoo Enterprise para la versión $odoo_version..."
-    gh repo clone odoo/enterprise -- -b "$odoo_version" /opt/odoo/source/enterprise
-    echo "Repositorio clonado exitosamente en /opt/odoo/source/enterprise."
+echo -e "\n"
+case $edition_choice in
+    1)
+        echo "Has seleccionado Odoo Community."
+        ;;
+    2)
+        echo "Has seleccionado Odoo Enterprise. Por favor, asegúrate de tener un token de acceso personal de GitHub."
+        read -p "Introduce tu token de acceso personal de GitHub: " github_pat
+        echo -e "\nClonando el repositorio de Odoo Enterprise..."
+        sudo git clone https://$github_pat@github.com/odoo/enterprise -b $odoo_version /opt/odoo/source/enterprise
+        if [ $? -eq 0 ]; then
+            echo -e "\n\033[1;32mRepositorio de Odoo Enterprise clonado exitosamente.\033[0m"
+        else
+            echo -e "\033[1;31mNo se pudo clonar el repositorio Enterprise. Verifica tu token y acceso al repositorio.\033[0m"
+            exit 1
+        fi
+        ;;
+    *)
+        echo -e "\033[1;31mOpción no válida. Por favor, intenta nuevamente.\033[0m"
+        exit 1
+        ;;
+esac
+
+echo -e "\nClonando el repositorio de Odoo Community..."
+sudo git clone https://github.com/odoo/odoo -b $odoo_version /opt/odoo/source/odoo
+if [ $? -eq 0 ]; then
+    echo -e "\n\033[1;32mRepositorio de Odoo clonado exitosamente en /opt/odoo/source/odoo.\033[0m"
+else
+    echo -e "\033[1;31mError al clonar el repositorio de Odoo. Verifica los permisos y el espacio disponible.\033[0m"
 fi
 
-# Clonar el repositorio de Odoo
-echo "Clonando el repositorio de Odoo para la versión $odoo_version..."
-gh repo clone odoo/odoo -- -b "$odoo_version" /opt/odoo/source/odoo
-echo "Repositorio de Odoo clonado exitosamente en /opt/odoo/source/odoo."
-
-echo "El proceso de instalación ha finalizado. Por favor, personaliza tu instalación de Odoo según tus necesidades específicas."
+echo -e "\n\033[1;32mEl proceso de instalación ha finalizado. Por favor, personaliza tu instalación de Odoo según tus necesidades específicas.\033[0m"
